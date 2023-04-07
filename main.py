30f2f70d993991c45bc64f9dd8f9c50a340d3d93
cgmark101 <cgmark101@gmail.com>
2023-04-07 15:35:14 -0400

    update

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ def get_service_status(service_name: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
