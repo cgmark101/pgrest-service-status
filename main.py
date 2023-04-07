3e87c953d023d50de2e7ac283d7fe7dae8cd263b
cgmark101 <cgmark101@gmail.com>
2023-04-07 13:40:38 -0400

    update

--- main.py
+++ main.py
@@ -9,13 +9,7 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 templates = Jinja2Templates(directory="templates")
-
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
 
 
 @app.get("/status", response_class=HTMLResponse)
